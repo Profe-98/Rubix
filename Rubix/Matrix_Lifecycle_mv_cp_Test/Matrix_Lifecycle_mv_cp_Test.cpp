f7d5dc986a0c947a6537bc2937900fd51637fb65
profe98 <profe98@web.de>
2024-08-08 16:11:16 +0200

    setting up unit test(lets hope it works)

---
+++ Rubix/Matrix_Lifecycle_mv_cp_Test/Matrix_Lifecycle_mv_cp_Test.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace MatrixLifecyclemvcpTest
+{
+	TEST_CLASS(MatrixLifecyclemvcpTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
